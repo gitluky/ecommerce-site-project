4347b758069b53fed8071a0a657a28429c53d3c1
gitluky <jluk87@gmail.com>
2019-10-25 14:54:30 -0400

    Generated Category resource.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
   has_many_attached :product_images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :products
   root 'application#index'
   devise_for :users
