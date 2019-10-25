dd69452d8414af6380d01c8ed492e1668d7a7968
gitluky <jluk87@gmail.com>
2019-10-25 14:59:40 -0400

    Created migration product_categories, and defined the relationships between Product and Category.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
+  has_many :product_categories
+  has_many :categories, through: :product_categories
   has_many_attached :product_images
 end
